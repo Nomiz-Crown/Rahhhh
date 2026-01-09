2e894464e9a553ef5f3044d641864e923a62ec8a
KevinLusych <kevin.lustych@elev.ga.lbs.se>
2026-01-09 12:05:25 +0100

    n
    n

--- Tower Defence Beta/Assets/loadscene.cs
+++ Tower Defence Beta/Assets/loadscene.cs
@@ -3,18 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class loadscene : MonoBehaviour
 {
+    [SerializeField] Animator transitionanim;
+    private void Start()
+    {
+        Debug.Log("loadscene script active on " + gameObject.name);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("2D Trigger entered by: " + other.name);
 
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player detected! Loading scene Zone1");
-            SceneManager.LoadScene("Zone1");
+            Debug.Log("Player detected! Starting scene load coroutine");
+            StartCoroutine(LoadSceneAfterDelay());
         }
-        else
-        {
-            Debug.Log("Object is not tagged Player");
-        }
+    }
+
+    private System.Collections.IEnumerator LoadSceneAfterDelay()
+    {
+        Debug.Log("Waiting 1 seconds before loading scene...");
+        yield return new WaitForSeconds(1f);
+       
+        Debug.Log("Loading scene Zone1 now");
+        SceneManager.LoadScene("Zone1");
+        transitionanim.SetTrigger("start");
+
     }
 }
