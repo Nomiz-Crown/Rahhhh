53d5216834dab517627b67989fb85ad64a5b892c
Lord1Murad <Muradiksweden@gmail.com>
2026-01-15 09:47:01 +0100

    psuhh gold

--- Tower Defence Beta/Assets/Codes/Gold/Gold.cs
+++ Tower Defence Beta/Assets/Codes/Gold/Gold.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Gold : MonoBehaviour
 {
-    public int GoldCoinHave;
-    public Sprite GoldCoin; 
-
+    public int GoldCoinHave = 0;
+    public Sprite GoldCoin;
+    public TextMeshProUGUI goldText;
     // Start is called before the first frame update
     void Start()
     {
-        GoldCoinHave = 0; 
-
+        UpdateGoldUI();
     }
-
+    public void AddGold(int amount)
+    {
+        GoldCoinHave += amount;
+        UpdateGoldUI();
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+    void UpdateGoldUI()
+    {
+        goldText.text = GoldCoinHave.ToString();
+    }
+
 }
